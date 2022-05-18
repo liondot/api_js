ab3cc8bed53a560fc84b17a0d4307d0aec9e898f
liondot <51257576+liondot@users.noreply.github.com>
2022-05-18 13:18:44 +0900

    해당 탭영역 에러 찾아야함.....

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 
 let news = []
+const menus = document.querySelectorAll(".menus button")
+menus.forEach((menus) => menus.addEventListener("click", (event) => getNewsByTopic(event)));
+
 const getLatestNews = async() => {
   let url = new URL (
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10`
   );
   let header = new Headers({'x-api-key':'YyyhWHPrmOjfy1L6CSxpnoxdXI9Pr1LRkq9eeUR5eVA'})
 
-  let response = await fetch(url,{headers:header})
+  let response = await fetch(url, {headers:header})
   let data = await response.json();
   news = data.articles;
   console.log(news);
@@ -15,11 +18,22 @@ const getLatestNews = async() => {
   
 };
 
+const getNewsByTopic = async (event) => {
+  let topic = event.target.textContent.toLowerCase()
+  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10&topic=${topic}`)
+
+  let header = new Headers({"x-api-key":'YyyhWHPrmOjfy1L6CSxpnoxdXI9Pr1LRkq9eeUR5eVA'})
+  let response = await fetch(url, {headers: header});
+  let data = await response.json();
+  news = data.articles;
+  render();
+};
+
 const render = () => {
-  let newsHTML = ''
+  let newsHTML = '';
 
   newsHTML = news.map((item) => {
-    return `
+    return ` 
     <div class="row news">
     <div class="col-lg-4">
       <img class="img-size" src="${item.media}">
@@ -33,7 +47,6 @@ const render = () => {
     ` 
   }).join('');
 
-  console.log(newsHTML)
 
   document.getElementById("news-board").innerHTML = newsHTML
 
