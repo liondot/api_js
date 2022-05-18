9901b6a0c868a7af3aac234da17c153f3801e044
liondot <51257576+liondot@users.noreply.github.com>
2022-05-18 12:13:23 +0900

    api 그러주는 강의

--- main.js
+++ main.js
@@ -7,11 +7,36 @@ const getLatestNews = async() => {
   let header = new Headers({'x-api-key':'YyyhWHPrmOjfy1L6CSxpnoxdXI9Pr1LRkq9eeUR5eVA'})
 
   let response = await fetch(url,{headers:header})
-  let data = await response.json()
-  news = data.articles
-  console.log(news)
-  console.log(data)
-  console.log(response)
+  let data = await response.json();
+  news = data.articles;
+  console.log(news);
+
+  render()
+  
 };
+
+const render = () => {
+  let newsHTML = ''
+
+  newsHTML = news.map((item) => {
+    return `
+    <div class="row news">
+    <div class="col-lg-4">
+      <img class="img-size" src="${item.media}">
+    </div>
+    <div class="col-lg-8">
+      <h2>${item.title}</h2>
+      <p>${item.summary}</p>
+      <div>${item.rights} * ${item.published_date}</div>
+    </div>
+  </div>
+    ` 
+  }).join('');
+
+  console.log(newsHTML)
+
+  document.getElementById("news-board").innerHTML = newsHTML
+
+}
 
 getLatestNews();
