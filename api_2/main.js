44f41b72954ebbdba615aa4c12aa980816508fe6
liondot <51257576+liondot@users.noreply.github.com>
2022-05-20 16:29:21 +0900

    카테고리 에러 확인

--- api_2/main.js
+++ api_2/main.js
@@ -1,18 +1,35 @@
 
 let news = [];
+let menus = document.querySelectorAll(".menus button")
+menus.forEach((menu) => menu.addEventListener('click', (event)=> getNewsByTopic(event)))
+
 const getLatestNews = async() => {
-  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10`)
+  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10`)
   let header = new Headers({
     'x-api-key':'YyyhWHPrmOjfy1L6CSxpnoxdXI9Pr1LRkq9eeUR5eVA'
   })
-  let response = await fetch(url,{headers:header})
+  let response = await fetch(url, {headers:header})
   let data = await response.json();
   console.log("this is data", data)
   news = data.articles;
-  console.log(news)
 
   render()
 };
+
+const getNewsByTopic = async(event) => {
+  let topic = event.target.textContent.toLowerCase()
+  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10=${topic}`)
+  let header = new Headers({
+    'x-api-key':'YyyhWHPrmOjfy1L6CSxpnoxdXI9Pr1LRkq9eeUR5eVA'
+  })
+  let response = await fetch(url, {headers:header});
+  let data = await response.json();
+  news = data.articles;
+
+  render()
+
+  console.log("토픽뉴스 데이터", data)
+} 
 
 const render = () => {
   let newsHTML = ''
@@ -29,8 +46,6 @@ const render = () => {
     </div>
   </div>`
   }).join('');
-
-
 
   document.getElementById("news-board").innerHTML = newsHTML
 }
