5c531b7ee4092b111edae1cd0a3a3b313804e083
liondot <51257576+liondot@users.noreply.github.com>
2022-07-16 20:36:45 +0900

    data에 있는 자료를 갖고 오는 news = data.articles 입력

--- api_2/main.js
+++ api_2/main.js
@@ -1,6 +1,14 @@
-const getLatestNews = () => {
-  let url = 'https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10'
-  console.log(url)
+
+let news = [];
+const getLatestNews = async() => {
+  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10`)
+  let header = new Headers({"x-api-key": "7e5MmopN4uLr2-sEBt-sYPzqM57FC8TBYZJjjQkXNXk",})
+  let response = await fetch(url, { headers: header }) //데이터를 보내는 방식은 여러개가 있는데 ajax, http, fetch가 있다. 
+  // 위에 사용한 fetch는 서버와 통신을 해야 돼서 기다려야 함 
+  let data = await response.json()
+  console.log("this is data",data)
+  news = data.articles;
+  console.log(news)
 }
 
 getLatestNews();
