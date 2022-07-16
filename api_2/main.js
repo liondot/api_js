ed2efcf4a3aa0ae33000d7b7e112377da085d02e
liondot <51257576+liondot@users.noreply.github.com>
2022-07-16 20:19:49 +0900

    api url,header의 키값을 자바스크립트 클래스로 변수 선언

--- api_2/main.js
+++ api_2/main.js
@@ -1,53 +1,6 @@
-
-let news = [];
-let menus = document.querySelectorAll(".menus button")
-menus.forEach((menu) => menu.addEventListener('click', (event)=> getNewsByTopic(event)))
-
-const getLatestNews = async() => {
-  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10`)
-  let header = new Headers({
-    'x-api-key':'YyyhWHPrmOjfy1L6CSxpnoxdXI9Pr1LRkq9eeUR5eVA'
-  })
-  let response = await fetch(url, {headers:header})
-  let data = await response.json();
-  console.log("this is data", data)
-  news = data.articles;
-
-  render()
-};
-
-const getNewsByTopic = async(event) => {
-  let topic = event.target.textContent.toLowerCase()
-  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10=${topic}`)
-  let header = new Headers({
-    'x-api-key':'YyyhWHPrmOjfy1L6CSxpnoxdXI9Pr1LRkq9eeUR5eVA'
-  })
-  let response = await fetch(url, {headers:header})
-  let data = await response.json();
-  news = data.articles;
-
-  render()
-
-  console.log("토픽뉴스 데이터", data)
-} 
-
-const render = () => {
-  let newsHTML = ''
-
-  newsHTML = news.map((item) => {
-    return `<div class="row news">
-    <div class="col-lg-4">
-      <img class="news-img-size" src="${item.media}" alt="">
-    </div>
-    <div class="col-lg-8">
-      <h2>${item.title}</h2>
-      <p>${item.summary}</p>
-      <div>${item.clean_url} ${item.published_date}</div>
-    </div>
-  </div>`
-  }).join('');
-
-  document.getElementById("news-board").innerHTML = newsHTML
+const getLatestNews = () => {
+  let url = 'https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10'
+  console.log(url)
 }
 
-getLatestNews()
+getLatestNews();
