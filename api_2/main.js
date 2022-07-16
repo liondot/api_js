4ceff587afdb888b20771248546e824ea6f5f9ef
liondot <51257576+liondot@users.noreply.github.com>
2022-07-16 21:17:49 +0900

    render 함수로 페이지 ui작업

--- api_2/main.js
+++ api_2/main.js
@@ -9,6 +9,29 @@ const getLatestNews = async() => {
   console.log("this is data",data)
   news = data.articles;
   console.log(news)
+
+  render()
 }
 
 getLatestNews();
+
+const render = () => {
+  let newsHTML = ''
+  newsHTML = news.map((news) => {
+    return `<div class="row news">
+    <div class="col-lg-4">
+        <img class="new_img_size" src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA3MDJfMjMx%2FMDAxNjU2NzY3MTUwMTgw.8a_kJ25Zw-GCeIqH-GpEdiALZai2JOZoC1CqdE7QQ6wg.tvC6DVDMQAfltaMPAjZBAbHUTFuX0HTd-wEeHXQZCvcg.JPEG.cony0905%2F%25C0%25CC%25BB%25F3%25C7%25D1%25BA%25AF%25C8%25A3%25BB%25E7_%25BF%25EC%25BF%25B5%25BF%25EC_1%25C8%25AD_%252823%2529.jpg&type=sc960_832" alt="">
+    </div>
+    <div class="col-lg-8">
+        <h2>이상한 변호사 우영우’ 박은빈, 포스터 비하인드컷 공개
+        </h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste obcaecati necessitatibus nisi dolorum praesentium modi sapiente quam minus similique odio.</p>
+        <div>
+        KBS 2022.07.10
+        </div>
+    </div>
+</div>`
+  }).join('')
+
+  document.getElementById("news_board").innerHTML = newsHTML;
+}
